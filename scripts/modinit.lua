eefe2f466db6f12e70194c4c2e7c7d2211f8aecd
Phyrax <124089724+Phyrax1@users.noreply.github.com>
2025-11-14 14:11:26 +0100

    Update modinit.lua

--- scripts/modinit.lua
+++ scripts/modinit.lua
@@ -99,6 +99,7 @@ local function init( modApi )
 	include( scriptPath .. "/btree/conditions" )
 	include( scriptPath .. "/btree/bountytargetbrain" )
 	include( scriptPath .. "/btree/bountyfakebrain" )
+	include(scriptPath .. "/appended_functions/abilities/disguise")
 
 	include( scriptPath.."/hud/hud" )--from Interactive Events, required for modal dialog choice menu to work properly
 
