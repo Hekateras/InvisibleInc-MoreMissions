292f3618bd9e0f6cbbc6d0045b7f33a16cb4a965
Phyrax <124089724+Phyrax1@users.noreply.github.com>
2026-01-19 00:47:15 +0100

    Change append to simfactory for upgrade screen

---
+++ scripts/appended_functions/simfactory.lua
@@ -0,0 +1,43 @@
+local simfactory = include("sim/simfactory")
+
+local oldcreateUnit = simfactory.createUnit
+function simfactory.createUnit(unitData, ...)
+	local unit = oldcreateUnit(unitData, ...)
+	local traits = unit:getTraits()
+	if traits then
+		if traits.MM_mod_cooldownMax and traits.cooldownMax then
+			traits.cooldownMax = traits.cooldownMax + traits.MM_mod_cooldownMax
+		end
+		if traits.MM_mod_chargesMax and traits.chargesMax then
+			traits.chargesMax = traits.chargesMax + traits.MM_mod_chargesMax
+			traits.charges = traits.charges + traits.MM_mod_chargesMax
+		end
+		if traits.MM_mod_maxAmmo and traits.maxAmmo then
+			traits.maxAmmo = traits.maxAmmo + traits.MM_mod_maxAmmo
+			if traits.ammo_clip then
+				traits.ammo_clip = traits.ammo_clip + traits.MM_mod_maxAmmo
+			end
+		end
+		if traits.MM_mod_pwrCost and traits.pwrCost then
+			traits.pwrCost = traits.pwrCost + traits.MM_mod_pwrCost
+		end
+		if traits.MM_mod_requirements then
+			unit:getUnitData().requirements = nil
+		end
+		if traits.MM_mod_armorPiercing then
+			traits.armorPiercing = (traits.armorPiercing or 0) + traits.MM_mod_armorPiercing
+		end
+		if traits.MM_mod_damage then
+			if traits.damage and traits.stun then
+				traits.stun = traits.stun + traits.MM_mod_damage
+			end
+			if traits.damage then
+				traits.damage = traits.damage + traits.MM_mod_damage
+			end
+			if traits.baseDamage then
+				traits.baseDamage = traits.baseDamage + traits.MM_mod_damage
+			end
+		end
+	end
+	return unit
+end
