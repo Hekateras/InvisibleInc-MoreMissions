a946fcb62c395b3f7848f88331d52e557d152082
Phyrax <124089724+Phyrax1@users.noreply.github.com>
2026-03-01 16:13:58 +0100

    Change talking head spool to turn page if text is too long

--- scripts/appended_functions/talkingheadfix.lua
+++ scripts/appended_functions/talkingheadfix.lua
@@ -2,6 +2,120 @@ local cdefs = include( "client_defs" )
 local rig_util = include( "gameplay/rig_util" )
 local talkinghead_ingame = include( "client/fe/talkinghead_ingame" )
 
+local ShowLine = talkinghead_ingame.ShowLine
+talkinghead_ingame.ShowLine = function(self, idx, ...)
+	if not self.script[idx].MM_script then
+		return ShowLine(self, idx, ...)
+	end
+
+	self.line_idx = idx
+
+	if self._scriptThread then
+		self._scriptThread:stop()
+		self._scriptThread = nil
+	end
+
+	self:StopLine(self)
+
+	local line = self.script[idx]
+	self.line = line
+
+	local was_viz = self.widget:isVisible()
+	if (self:shouldShowSubtitles() or not line.voice) and (not self.ismainframefn or not self.ismainframefn()) then
+		self.widget:setVisible(true)
+	else
+		self.widget:setVisible(false)
+	end
+
+	local playing_voice = false
+	if line.voice then
+		MOAIFmodDesigner.playSound(line.voice, "talkinghead_voice")
+		playing_voice = MOAIFmodDesigner.isPlaying("talkinghead_voice")
+	else
+		MOAIFmodDesigner.playSound("SpySociety/HUD/gameplay/Operator/textbox")
+	end
+
+	if
+		self.widget:isVisible()
+		and (not was_viz or line.anim ~= self.talking_head_anim or self.line_idx == 1)
+		and not self.widget:hasTransition()
+	then
+		self.widget:createTransition("activate_left")
+	end
+	self.talking_head_anim = line.anim
+
+	self.widget.binder.profileAnim:bindBuild(line.build or line.anim)
+	self.widget.binder.profileAnim:bindAnim(line.anim)
+	if line.name then
+		self.widget.binder.headerTxt:setText(line.name .. ":")
+	else
+		self.widget.binder.headerTxt:setText("")
+	end
+
+	-- new code starts here
+	local lbl = self.widget.binder.bodyTxt
+	-- why was this faster with no voice? makes no sense to me
+	lbl:spoolText(line.text, 30) -- line.voice and 30 or 60)
+	self._typeThread = MOAICoroutine.new()
+	self._typeThread:run(function()
+		while true do
+			lbl._cont:getProp():spool()
+			while lbl:isSpooling() do
+				coroutine.yield()
+			end
+			rig_util.wait(2.5 * cdefs.SECONDS)
+			if lbl:hasNextPage() then
+				lbl:nextPage()
+			else
+				break
+			end
+		end
+		self._typeThread = nil
+	end)
+
+	if self.line_idx < #self.script then
+		self._scriptThread = MOAICoroutine.new()
+		self._scriptThread:run(function()
+			if playing_voice then
+				while MOAIFmodDesigner.isPlaying("talkinghead_voice") do
+					coroutine.yield()
+				end
+				rig_util.wait(0.2 * cdefs.SECONDS)
+			else
+				-- wait for typethread to terminate instead of a fixed amount of time
+				while self._typeThread ~= nil do
+					coroutine.yield()
+				end
+			end
+
+			self:ShowLine(self.line_idx + 1)
+		end)
+	elseif self.line_idx >= #self.script then
+		self._scriptThread = MOAICoroutine.new()
+		self._scriptThread:run(function()
+			if playing_voice then
+				while MOAIFmodDesigner.isPlaying("talkinghead_voice") do
+					coroutine.yield()
+				end
+				rig_util.wait(0.2 * cdefs.SECONDS)
+			else
+				-- wait for typethread to terminate instead of a fixed amount of time
+				while self._typeThread ~= nil do
+					coroutine.yield()
+				end
+			end
+
+			if self.onfinished then
+				self.onfinished()
+			end
+			self:Halt()
+			self.isdone = true
+		end)
+	end
+end
+
+--[[ -- prevent Operator messages during mainframe mode, if we want this
+
 --by Cyberboy2000
 
 local oldShowLine = talkinghead_ingame.ShowLine
@@ -30,4 +144,5 @@ function talkinghead_ingame:ShowLine(idx)
     end
     
     oldShowLine(self, idx)
-end
+
+end ]]--
